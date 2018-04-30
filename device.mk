adfc59ce2110235be67941e3bc24723d5f166e82
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-04-30 20:49:54 -0300

    cedric: LineageActions -> MotoActions
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -114,9 +114,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfig
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Display
 PRODUCT_PACKAGES += \
