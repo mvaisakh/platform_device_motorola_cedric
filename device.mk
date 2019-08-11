3a02a8bf886d891e329ec248e330ac8766b4f1df
Vaisakh Murali <vaisakhmurali@gmail.com>
2019-08-11 16:55:05 +0530

    cedric: uprev Thermal hals to 1.1
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -410,8 +410,8 @@ PRODUCT_PACKAGES += \
 
 #Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@1.1-impl \
+    android.hardware.thermal@1.1-service \
     thermal.msm8937
 
 #Wifi
