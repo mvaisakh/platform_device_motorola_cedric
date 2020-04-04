b42ac033a56d98a3debb4fae274f2f3be36ab23b
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-04-04 17:33:56 +0530

    [DNM] cedric: Build permissive
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,6 +74,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=30
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += vmalloc=350M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
